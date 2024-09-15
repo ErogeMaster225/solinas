96ec974f4f8305bbb6840662708a122cd1b7e122
Nguyễn Trọng Sang <33416398+ErogeMaster225@users.noreply.github.com>
2024-09-15 13:43:41 +0700

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger"
+import { Elysia } from "elysia"
+import { Innertube } from "youtubei.js/web"
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const youtube = await Innertube.create()
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+const app = new Elysia()
+    .use(swagger())
+    .get("/", () => "Hello Elysia")
+    .get("/youtube/:id", async ({ params: { id } }) => {
+        const data = await youtube.getBasicInfo(id)
+        return data.basic_info
+    })
+    .listen(Bun.env.ELYSIA_PORT ?? 3000)
+
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
